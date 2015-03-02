733864b5349826ef18bb846eac99be3a3f1ad9bd
Gabriel C.- <gabrielcarrillolopez@gmail.com>
2015-03-02 14:59:25 +0100

    2semana

---
+++ 2semana/Mapa mental M3.1.mm
@@ -0,0 +1,207 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1424952682914" ID="ID_615761805" MODIFIED="1424956238789">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      <font color="#cc0066">Conceptos clave en el </font>
+    </p>
+    <p style="text-align: center">
+      <font color="#cc0066">COMERCIO ELECTR&#211;NICO</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1424954839522" ID="ID_68482058" MODIFIED="1424954896018" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#00cc33">MERCADOS DIGITALES</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1424954940089" ID="ID_529857531" MODIFIED="1424955544230">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      <font color="#0099ff">Reducci&#243;n de la ASIMETR&#205;A DE LA INFORMACI&#211;N</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1424955018392" ID="ID_1182597088" MODIFIED="1424955280450">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Reducci&#243;n costes de men&#250;:</b>
+    </p>
+    <p>
+      <font size="2">capacidad del comerciante </font>
+    </p>
+    <p>
+      <font size="2">de ajustar precios</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1424955164836" ID="ID_1959977395" MODIFIED="1424955274070">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Discriminaci&#243;n de precios</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1424955187480" ID="ID_324797604" MODIFIED="1424955257939">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Ajuste din&#225;mico de precios</b>
+    </p>
+    <p>
+      <font size="2">en funci&#243;n de las </font>
+    </p>
+    <p>
+      <font size="2">condiciones del mercado</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1424955318025" ID="ID_846348256" MODIFIED="1424955718282">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Reducci&#243;n de costes de </b>
+    </p>
+    <p>
+      <b>transacci&#243;n de compra:</b>
+    </p>
+    <p>
+      <font size="2">desintermediaci&#243;n</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+<node CREATED="1424954897555" ID="ID_200081983" MODIFIED="1424954916158" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#00cc33">BIENES DIGITALES</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1424955802444" ID="ID_298769778" MODIFIED="1424955915663">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0099ff">Ditribu&#237;bles mediante internet (m&#250;sica, v&#237;deo, ...)</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1424955918164" ID="ID_1004305430" MODIFIED="1424956010940">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Coste de copia: <font color="#ff6666">0</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1424956012337" ID="ID_983591293" MODIFIED="1424956075481">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Coste de entrega: <font color="#ff6666">Bajo</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1424956109626" ID="ID_1017220433" MODIFIED="1424956119582">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Coste de inventario: <font color="#ff6666">Bajo</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1424956076450" ID="ID_1861592442" MODIFIED="1424956101514">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Coste de m&#225;rketing: Igual
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1424956154235" ID="ID_1815150170" MODIFIED="1424956202833">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Coste final: <font color="#ff6666">Variable</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</map>
