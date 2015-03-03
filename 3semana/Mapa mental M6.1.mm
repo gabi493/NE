e009781c53a097e113b51263870fca2808d64b8d
Gabriel C.- <gabrielcarrillolopez@gmail.com>
2015-03-03 11:06:42 +0100

    3semana

---
+++ 3semana/Mapa mental M6.1.mm
@@ -0,0 +1,536 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1425308402404" ID="ID_1119342470" MODIFIED="1425308663325">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      <font color="#00cc33"><b>SISTEMAS INTEGRALES </b></font>
+    </p>
+    <p style="text-align: center">
+      <font color="#00cc33"><b>DE EMPRESA</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1425308666465" ID="ID_778811870" MODIFIED="1425312748175" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0099cc"><b>Aplicaciones empresariales: </b></font>
+    </p>
+    <p>
+      <font color="#000000" size="2">sistemas que abarcan todas las &#225;reas </font>
+    </p>
+    <p>
+      <font color="#000000" size="2">funcionales para ejecutar procesos </font>
+    </p>
+    <p>
+      <font color="#000000" size="2">de negocio a lo largo de la empresa</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1425311428073" ID="ID_1452558144" MODIFIED="1425312991581">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#ff6666"><b>Sistemas empresariales (ERP)</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1425313085714" ID="ID_1797474590" MODIFIED="1425313510571">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#006666">Recopilan y almacenan en una BD la info </font>
+    </p>
+    <p>
+      <font color="#006666">de varios procesos de negocios clave</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1425313162046" ID="ID_1439742753" MODIFIED="1425313176790" TEXT="manufactura y producci&#xf3;n"/>
+<node CREATED="1425313216154" ID="ID_1599376782" MODIFIED="1425313221688" TEXT="finanzas y contabilidad"/>
+<node CREATED="1425313178839" ID="ID_1687445925" MODIFIED="1425313193833" TEXT="ventas y marketing"/>
+<node CREATED="1425313197469" ID="ID_693388077" MODIFIED="1425313241130">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      recursos humanos
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1425313401265" ID="ID_1117384735" MODIFIED="1425313494450">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#006666">Aceleran la comunicaci&#243;n de </font>
+    </p>
+    <p>
+      <font color="#006666">info en toda la compa&#241;&#237;a</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1425313527585" ID="ID_449370947" MODIFIED="1425313563863">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      facilita la coordinaci&#243;n de
+    </p>
+    <p>
+      sus operaciones cotidianas
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1425313585764" ID="ID_67978616" MODIFIED="1425313713039">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      automatiza el flujo de
+    </p>
+    <p>
+      datos a nivel interno
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1425313776427" ID="ID_1050590100" MODIFIED="1425313823685">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      posibilita la respuesta r&#225;pida
+    </p>
+    <p>
+      a las solicitudes de los clientes
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1425311484694" ID="ID_1917606881" MODIFIED="1425311521631">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#ff6666"><b>Sistemas de administraci&#243;n de </b></font>
+    </p>
+    <p>
+      <font color="#ff6666"><b>la cadena de suministro (SCM)</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1425314009334" ID="ID_785674747" MODIFIED="1425370618427">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#006666">Ayudan a manejar las relaciones con los </font>
+    </p>
+    <p>
+      <font color="#006666">PROVEEDORES compartiendo informaci&#243;n de:</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1425339354752" ID="ID_774482763" MODIFIED="1425339359933" TEXT="pedidos"/>
+<node CREATED="1425339361613" ID="ID_852775510" MODIFIED="1425339364175" TEXT="productos"/>
+<node CREATED="1425339364446" ID="ID_1531776745" MODIFIED="1425339381253" TEXT="niveles de inventario"/>
+<node CREATED="1425339381707" ID="ID_339495676" MODIFIED="1425339429641">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      entrega de productos
+    </p>
+    <p>
+      y servicios
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1425339636600" ID="ID_1637769428" MODIFIED="1425339681744">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#006666">Ayuda a las empresas a:</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1425339686755" ID="ID_914339550" MODIFIED="1425339811245">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      decidir cu&#225;ndo y qu&#233; producir,
+    </p>
+    <p>
+      almacenar y desplazar
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1425339733395" ID="ID_1584795237" MODIFIED="1425339735642" TEXT="comunicar pedidos r&#xe1;pidamente"/>
+<node CREATED="1425339775469" ID="ID_183851867" MODIFIED="1425339796168" TEXT="seguir el estado de los pedidos"/>
+<node CREATED="1425339819436" ID="ID_1690831805" MODIFIED="1425339916951">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      verificar la disponibilidad de inventario y supervisar sus niveles
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1425339888623" ID="ID_542814742" MODIFIED="1425339890736" TEXT="reducir costes de inventario, transporte y almacenamiento"/>
+<node CREATED="1425339919215" ID="ID_1887862394" MODIFIED="1425339928616" TEXT="seguir los embarques"/>
+<node CREATED="1425339928820" ID="ID_183708763" MODIFIED="1425339953360" TEXT="planear la producci&#xf3;n bas&#xe1;ndose en la demanda real de los clientes"/>
+<node CREATED="1425339953594" ID="ID_1196791914" MODIFIED="1425339974179" TEXT="comunicar r&#xe1;pidamente los cambios en el dise&#xf1;o del producto"/>
+</node>
+</node>
+<node CREATED="1425311523587" ID="ID_780578852" MODIFIED="1425311577035">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#ff6666"><b>Sistemas de administraci&#243;n de las </b></font>
+    </p>
+    <p>
+      <font color="#ff6666"><b>relaciones con el cliente (CRM)</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1425340235080" ID="ID_1538677528" MODIFIED="1425370598210">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#006666">Ayuda a administrar las relaciones </font>
+    </p>
+    <p>
+      <font color="#006666">con los CLIENTES aportando info de los </font>
+    </p>
+    <p>
+      <font color="#006666">procesos de negocios relacionados con:</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1425368806743" ID="ID_81820518" MODIFIED="1425370486744">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#225;rea de ventas
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1425368810492" ID="ID_1089596074" MODIFIED="1425370553895">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#225;rea de marketing
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1425370557295" ID="ID_874241239" MODIFIED="1425370565684" TEXT="&#xe1;rea de servicio al cliente"/>
+</node>
+<node CREATED="1425370353534" ID="ID_870252081" MODIFIED="1425370371294">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#006666">Ayuda a las empresas a:</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1425370380445" ID="ID_1633571165" MODIFIED="1425370703937">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      identificar, atraer y conservar clientes NUEVOS
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1425370390151" ID="ID_1831910939" MODIFIED="1425370391859" TEXT="dar mejor servicio a clientes ya EXISTENTES"/>
+</node>
+</node>
+<node CREATED="1425311582864" ID="ID_1449669293" MODIFIED="1425311613183">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#ff6666"><b>Sistemas de administraci&#243;n </b></font>
+    </p>
+    <p>
+      <font color="#ff6666"><b>del conocimiento (KMS)</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1425370734295" ID="ID_1410361864" MODIFIED="1425371049576">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#006666">Apoyan los procesos para:</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1425371054637" ID="ID_988739623" MODIFIED="1425371075404" TEXT="almacenar, distribuir y aplicar el conocimiento"/>
+<node CREATED="1425371076689" ID="ID_742932673" MODIFIED="1425371116355">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      generar nuevo conocimiento e integrarlo en la organizaci&#243;n
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1425371264004" ID="ID_1752127796" MODIFIED="1425371286895">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#006666">Ayuda a las empresas a:</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1425371288698" ID="ID_182043979" MODIFIED="1425371300133" TEXT="mejorar los procesos de negocios"/>
+<node CREATED="1425371300353" ID="ID_102424794" MODIFIED="1425371317472" TEXT="mejorar la toma de decisiones administrativas"/>
+<node CREATED="1425371317733" ID="ID_101081570" MODIFIED="1425371338599" TEXT="enlazarla con fuentes externas de conocimiento"/>
+</node>
+</node>
+</node>
+<node CREATED="1425311358775" ID="ID_167698882" MODIFIED="1425372641670" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0099cc"><b>Intranets y extranets: </b></font>
+    </p>
+    <p>
+      <font color="#000000" size="2">para las empresas que no cuentan </font>
+    </p>
+    <p>
+      <font color="#000000" size="2">con los recursos para invertir en </font>
+    </p>
+    <p>
+      <font color="#000000" size="2">aplicaciones empresariales</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1425371354322" ID="ID_1276809152" MODIFIED="1425371920683">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#ff6666"><b>Intranets </b></font>
+    </p>
+    <p>
+      <font size="2">Presenta info a los usuarios en un portal privado </font>
+    </p>
+    <p>
+      <font size="2">con un solo punto de acceso (interfaz Web)</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1425372419251" ID="ID_1318335295" MODIFIED="1425372438637">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#006666">Incluyen:</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1425372440179" ID="ID_1130755067" MODIFIED="1425372447782" TEXT="correo electr&#xf3;nico"/>
+<node CREATED="1425372448043" ID="ID_133966690" MODIFIED="1425372456208" TEXT="herramientas de colaboraci&#xf3;n"/>
+<node CREATED="1425372456448" ID="ID_998658957" MODIFIED="1425372487867" TEXT="herramientas de b&#xfa;squedas en los sistemas"/>
+<node CREATED="1425372488115" ID="ID_1222598023" MODIFIED="1425372495459" TEXT="documentos internos"/>
+</node>
+<node CREATED="1425371534807" ID="ID_470687555" MODIFIED="1425372412133">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#006666">Ayuda a las empresas a:</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1425371634314" ID="ID_1693873759" MODIFIED="1425371666788" TEXT="distribuir la informaci&#xf3;n a los empleados internamente"/>
+<node CREATED="1425371673542" ID="ID_1277019973" MODIFIED="1425371723794">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      depostiar las pol&#237;ticas, los programas&#160;&#160;y los datos corporativos
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1425371504713" ID="ID_782841013" MODIFIED="1425372008951">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#ff6666"><b>Extranets</b></font>
+    </p>
+    <p>
+      <font size="2">Intranets extendidas a usuarios </font>
+    </p>
+    <p>
+      <font size="2">autorizados ajenos a la empresa</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1425372048239" ID="ID_390030170" MODIFIED="1425372074786">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#006666">Agilizan el flujo de info entre:</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1425372076113" ID="ID_615645876" MODIFIED="1425372078711" TEXT="empresa"/>
+<node CREATED="1425372078952" ID="ID_449434651" MODIFIED="1425372082424" TEXT="proveedores"/>
+<node CREATED="1425372082923" ID="ID_1105677903" MODIFIED="1425372085640" TEXT="clientes"/>
+</node>
+<node CREATED="1425372145677" ID="ID_1398778802" MODIFIED="1425372184430">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#006666">Permiten a empresas diferentes </font>
+    </p>
+    <p>
+      <font color="#006666">trabajar colaborativamente en:</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1425372185881" ID="ID_1294936819" MODIFIED="1425372192888" TEXT="dise&#xf1;o de productos"/>
+<node CREATED="1425372193148" ID="ID_1768828024" MODIFIED="1425372199345" TEXT="marketing"/>
+<node CREATED="1425372199553" ID="ID_1653900181" MODIFIED="1425372202619" TEXT="producci&#xf3;n"/>
+</node>
+</node>
+</node>
+</node>
+</map>
