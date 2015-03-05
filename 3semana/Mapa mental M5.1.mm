eb66c08855a63cf8b33457e7080ed8f39418b430
Gabriel C.- <gabrielcarrillolopez@gmail.com>
2015-03-05 15:09:48 +0100

    Mapa Mental M5.1

---
+++ 3semana/Mapa mental M5.1.mm
@@ -0,0 +1,543 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1425524936878" ID="ID_1489655470" MODIFIED="1425559777367">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font color="#009933">COMERCIO ELECTR&#211;NICO</font></b>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1425559794996" ID="ID_1624775288" MODIFIED="1425560565144" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#990033">Tipos</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1425559951308" ID="ID_1267506320" MODIFIED="1425560106253">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0099cc">Negocio a Consumidor (<b>B2C</b>)</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1425560313348" ID="ID_393995111" MODIFIED="1425560398631">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Venta de <i>productos</i>&#160;y <i>servicios</i>
+    </p>
+    <p>
+      a compradores <b>individuales</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1425560040217" ID="ID_388659168" MODIFIED="1425560099627">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0099cc">Negocio a Negocio (<b>B2B</b>)</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1425560326364" ID="ID_383902472" MODIFIED="1425560481703">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Venta de <i>bienes</i>&#160;y <i>servicios</i>
+    </p>
+    <p>
+      entre <b>empresas</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1425560059365" ID="ID_1979384482" MODIFIED="1425560092123">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0099cc">Consumidor a Consumidor (<b>C2C</b>)</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1425560485303" ID="ID_1277869457" MODIFIED="1425560536613">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Venta de <i>productos</i>
+    </p>
+    <p>
+      entre <b>consumidores</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1425559889457" ID="ID_679870703" MODIFIED="1425559912853" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#990033">Logro de las relaciones con el cliente</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1425562525751" ID="ID_462372199" MODIFIED="1425562571857">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0099cc">Herramientas de m&#225;rketing</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1425560150737" ID="ID_845402743" MODIFIED="1425562592229">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0066cc">Marketing interactivo y personalizaci&#243;n</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1425560884436" ID="ID_459481215" MODIFIED="1425561068888">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <u>crear</u>&#160;productos <i>personalizados</i>
+    </p>
+    <p>
+      para <b>millones</b>&#160;de consumidores
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1425560983006" ID="ID_1050141430" MODIFIED="1425561374578">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <u>obtener</u>&#160;informaci&#243;n <i>detallada</i>
+    </p>
+    <p>
+      de <b>cada</b>&#160;cliente:
+    </p>
+    <p>
+      - herramientas de seguimiento de la navegaci&#243;n
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1425561089838" ID="ID_173138333" MODIFIED="1425563705573">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#66cc00">comportamiento</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1425561093215" ID="ID_509792393" MODIFIED="1425563720613">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#66cc00">preferencias</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1425561095228" ID="ID_931066067" MODIFIED="1425563729266">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#66cc00">necesidades</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1425561100414" ID="ID_972394707" MODIFIED="1425563740469">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#66cc00">patrones de compras</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1425561178771" ID="ID_555690180" MODIFIED="1425561196420">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <u>adaptar</u>&#160;
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1425561201270" ID="ID_1160808162" MODIFIED="1425563751190">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#66cc00">promociones</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1425561208821" ID="ID_797820942" MODIFIED="1425563760010">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#66cc00">productos</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1425561210811" ID="ID_1644044066" MODIFIED="1425563769048">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#66cc00">servicios</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1425561213365" ID="ID_197817275" MODIFIED="1425563775819">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#66cc00">precios</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1425561702394" ID="ID_759157482" MODIFIED="1425561826874">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <u>crear</u>&#160;p&#225;ginas Web <i>personalizadas</i>
+    </p>
+    <p>
+      y &#250;nicas:
+    </p>
+    <p>
+      - filtrado colaborativo
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1425561830263" ID="ID_1224932926" MODIFIED="1425563787745">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <u><font color="#66cc00">comparar</font></u><font color="#66cc00">&#160;informaci&#243;n <i>recopilada</i> </font>
+    </p>
+    <p>
+      <font color="#66cc00">de <b>cada</b>&#160;cliente con informaci&#243;n </font>
+    </p>
+    <p>
+      <font color="#66cc00">de clientes con <i><b>intereses similares</b></i></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1425562215973" ID="ID_807167443" MODIFIED="1425563795888">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <u><font color="#66cc00">recomendar</font></u><font color="#66cc00">&#160;productos </font>
+    </p>
+    <p>
+      <font color="#66cc00">a <b>cada</b>&#160;cliente en funci&#243;n </font>
+    </p>
+    <p>
+      <font color="#66cc00">de <i><b>sus intereses</b></i></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1425562600894" ID="ID_668709876" MODIFIED="1425562623145">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0066cc">Blogs</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1425563029704" ID="ID_1576061159" MODIFIED="1425563073068">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Pueden incluir
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1425563074532" ID="ID_1198798166" MODIFIED="1425563808590">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#66cc00">Blogroll: colecci&#243;n de </font>
+    </p>
+    <p>
+      <font color="#66cc00">enlaces a <b>otros</b>&#160;blogs</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1425563155738" ID="ID_1199314596" MODIFIED="1425563814386">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#66cc00">Trackbacks: lista de art&#237;culos </font>
+    </p>
+    <p>
+      <font color="#66cc00">en <b>otros</b>&#160;blogs que <i>referencian</i>&#160; </font>
+    </p>
+    <p>
+      <font color="#66cc00">una publicaci&#243;n en <i><b>el primero</b></i></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1425563509443" ID="ID_665436964" MODIFIED="1425563519823" TEXT="Populares">
+<node CREATED="1425563519824" ID="ID_1157136224" MODIFIED="1425563959185">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#66cc00">Empresas: <i>canal</i>&#160;para <b>llegar</b>&#160;a los clientes</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1425563573083" ID="ID_87512486" MODIFIED="1425563991271">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#66cc00">Comerciantes: <i>forma</i>&#160;econ&#243;mica&#160;y </font>
+    </p>
+    <p>
+      <font color="#66cc00">r&#225;ida de <b>revisar</b>&#160;lo que se dice sobre</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1425563631517" ID="ID_1159550539" MODIFIED="1425563857436">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#990099">nuevos productos</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1425563640248" ID="ID_1669473564" MODIFIED="1425563866571">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#990099">viejas marcas</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1425563648944" ID="ID_886777746" MODIFIED="1425563873244">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#990099">campa&#241;as publicitarias</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1425560186773" ID="ID_320248446" MODIFIED="1425560199179">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0099cc">Autoservicio</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1425564109499" ID="ID_105581466" MODIFIED="1425564146676">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0066cc">Uso de Web y Mail</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1425564148503" ID="ID_158330597" MODIFIED="1425564204457">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <u>contestar</u>&#160;preguntas
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1425564165274" ID="ID_762382608" MODIFIED="1425564210466">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <u>ofrecer</u>&#160;informaci&#243;n
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
